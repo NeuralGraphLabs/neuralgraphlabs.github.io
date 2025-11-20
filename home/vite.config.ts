25e713564b4e8d71afc4f753577c35d0c263902c
Anirudh <anirudhxplorer@gmail.com>
2025-11-21 02:08:32 +0530

    update vite.config base

--- home/vite.config.ts
+++ home/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // Base: './' ensures assets are linked relatively.
   // This is crucial for GitHub Pages where your site might be at myuser.github.io/repo-name/
-  base: '/autodifflabs/', 
+  base: '/', 
   build: {
     outDir: 'dist',
     sourcemap: false,
